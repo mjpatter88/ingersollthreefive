97bee066661cf8dc154b130f7314a59cd19fdc9a
Michael Patterson <mjpatter88@gmail.com>
2016-08-21 22:29:55 -0500

    WIP: start supporting post requests.

--- website/views.py
+++ website/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contact
 
 def index(request):
-	# return HttpResponse("Hello Django. You're at the website index.")
-	return render(request, 'home.html')
+        if request.method == "POST":
+            print("POST received.")
+            c = Contact(name="Liz", email="lpatterson@gmail.com", waiting_list=True)
+            c.save()
+        return render(request, 'home.html')
