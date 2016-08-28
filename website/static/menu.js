14650c5371c4220b9a31b3d56cef145bfbd37032
Liz Patterson <lizpatterson630@gmail.com>
2016-08-28 18:18:50 -0500

    Image Gallery

--- website/static/menu.js
+++ website/static/menu.js
@@ -11,3 +11,36 @@ jQuery(function($){
   })
 })
 
+jQuery(function($){
+  $('#gallery').click(function(){
+      $('#container-slideshow').toggleClass('#container-slideshow-expand')
+  })
+})
+
+
+$(window).load(function(){
+		var pages = $('#container-slideshow li'), current=0;
+		var currentPage,nextPage;
+
+		$('#container-slideshow .button').click(function(){
+			currentPage= pages.eq(current);
+			if($(this).hasClass('prevButton'))
+			{
+
+				if (current <= 0)
+					current=pages.length-1;
+				else
+					current=current-1;
+			}
+			else
+			{
+				if (current >= pages.length-1)
+					current=0;
+				else
+					current=current+1;
+			}
+			nextPage = pages.eq(current);	
+			currentPage.hide();	
+			nextPage.show();		
+		});
+});
